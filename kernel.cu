3ff59db35f3c4d7d25d4d87ca0d27498a5ad3d88
MeredithHeskethFortescue <raphael.stascheit@study.hs-duesseldorf.de>
2019-09-25 17:15:48 +0200

    formatted according to code conventions

--- kernel.cu
+++ kernel.cu
@@ -16,22 +16,18 @@ texture<float, cudaTextureType2D> tex_kernel;
 __global__ void convolve(const int input_width,
                          const int input_height,
                          const int kernel_radius,
-                         float *output)
-{
+                         float *output) {
     // get coordinates by thread index
     int row = blockIdx.x * blockDim.x + threadIdx.x;
     int col = blockIdx.y * blockDim.y + threadIdx.y;
 
     // check if in range
-    if((col < input_height) && (row < input_width))
-    {
+    if ((col < input_height) && (row < input_width)) {
         float value = 0.0f;
 
         // iterate over the kernel matrix
-        for(int y = -kernel_radius; y <= kernel_radius; ++y)
-        {
-            for(int x = -kernel_radius; x <= kernel_radius; ++x)
-            {
+        for (int y = -kernel_radius; y <= kernel_radius; ++y) {
+            for (int x = -kernel_radius; x <= kernel_radius; ++x) {
                 // img value * kernel value
                 value += tex2D(tex_img, row + x, col + y) * tex2D(tex_kernel, x + kernel_radius, y + kernel_radius);
             }
